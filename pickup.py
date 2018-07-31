5fe44a0299e5917b9b7a016c1dc150288d66f38e
Ayumi Suzuki <ling.mu.bu@gmail.com>
2018-08-01 00:01:40 +0900

    Update pickup.py

--- pickup.py
+++ pickup.py
@@ -1,18 +1,16 @@
-#coding:utf-8
+#coding:utf-8                                                                                                       
 import glob
 import xlrd
 import os
 from datetime import datetime, timedelta
 
 def excel_date(num):
-  return (datetime(1899, 12, 30) + timedelta(days=num)).strftime('%Y/%m/%d')  
+  return (datetime(1899, 12, 30) + timedelta(days=num)).strftime('%Y/%m/%d')
 
-file_lst = glob.glob('./test/*.xls')
-for filename in file_lst:
-  book = xlrd.open_workbook(filename)
-  sheet = book.sheet_by_index(0)
-
+def makeReview(filename, sheet):
+  sheet = book.sheet_by_index(1)
   for r in range(2, sheet.nrows):
+    # B3                                                                                                            
     revision = sheet.cell_value(rowx=r, colx=1)
     comment = sheet.cell_value(rowx=r, colx=2).replace('\n', ' ')
     datestr = excel_date(sheet.cell_value(rowx=r, colx=3))
@@ -20,3 +18,16 @@ for filename in file_lst:
     str = "{} : {} \t {} \t {} \t {}".format(os.path.basename(filename), revision, datestr, name, comment)
     print(str)
 
+def checkTitle(filename, book):
+  sheet = book.sheet_by_index(0)
+
+  # C9                                                                                                              
+  title = sheet.cell_value(rowx=8, colx=2)
+  str = "{} \t {}".format(os.path.basename(filename), title)
+  print(str)
+
+file_lst = glob.glob('./test/*.xls')
+for filename in file_lst:
+  book = xlrd.open_workbook(filename)
+  checkTitle(filename, book)
+#  makeReview(filename, book)    
